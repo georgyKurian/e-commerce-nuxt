2ffa5327d76124196ede8fb0cac365d50211753a
Georgi Kurian <georgi@worksitesafety.ca>
2024-03-23 22:24:01 -0400

    Added blog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,7 @@ export default defineNuxtConfig({
     images: fileURLToPath(new URL('./assets', import.meta.url)),
   },
   modules: [
+    '@nuxt/content',
     '@nuxt/devtools',
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
