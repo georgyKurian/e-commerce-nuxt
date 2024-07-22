1003dee7bcbcc2e5b28143f20e3aa8a9b23c810d
Georgi Kurian <georgi@worksitesafety.ca>
2024-07-22 09:05:02 -0400

    Added billing types

---
+++ types/BillingAddress.ts
@@ -0,0 +1,13 @@
+export default interface BillingAddress {
+  firstName: string | null;
+  lastName: string | null;
+  companyName: string | null;
+  lineOne: String | null;
+  lineTwo: String | null;
+  lineThree: String | null;
+  city: String | null;
+  state: String | null;
+  postcode: String | null;
+  country: String | null;
+  phone: String | null;
+}
